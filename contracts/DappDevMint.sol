e63352e2148a69f53984accbe0cbb6db6d9f5300
Mike M <mmclaughlin@onestreamsoftware.com>
2018-03-22 12:17:15 -0400

    Changed delegates to mapping, added event for AccessControl
    changed delegates to mapping from array to avoid looping.  Added event when access control is changed.

--- contracts/DappDevMint.sol
+++ contracts/DappDevMint.sol
@@ -11,14 +11,20 @@ contract DappDevMint
   address public owner;
 
   /* addresses that have authority to mint */
-  address[] public delegates;
-
+  mapping (address => bool) public canMint;
+  
+  /* Event for minter access control */
+  event AccessControl(address minter, bool granted);
+  
+  
   function DappDevMint(address _token)
     public
   {
     token = DappDevToken(_token);
     owner = msg.sender;
+    canMint[msg.sender] = true;
   }
+
 
   function acceptOwnership()
     public
@@ -54,27 +60,19 @@ contract DappDevMint
     view
     returns (bool)
   {
-    // Search through delegates list
-    for (uint i = 0; i < delegates.length; i++)
-      if (candidate == delegates[i])
-        // Return true if candidate is delegate
-        return true;
-
-    // Not an owner or delegate
-    return false;
+    return canMint[candidate];
   }
 
 
   /**
    * @dev Function to mint tokens
    * @param _to The address that will receive the minted tokens.
-   * @param minterIndex The index of the minter in the delegates array
    * @param _amount The amount of tokens to mint.
    */
-  function mint(address _to, uint minterIndex, uint256 _amount)
+  function mint(address _to, uint256 _amount)
     public
   {
-      require(msg.sender == findByIndex(minterIndex));
+      require(canMint[msg.sender]);
       assert(token.mint(_to, _amount));
   }
 
@@ -95,9 +93,8 @@ contract DappDevMint
    function addDelegate(address newDelegate) public {
       //only owner can add delegate
       require(isOwner(msg.sender));
-      // don't add if already there
-      if (!isDelegate(newDelegate))
-         delegates.push(newDelegate);
+      canMint[newDelegate] = true;
+      emit AccessControl(newDelegate, true);
   }
 
   /**
@@ -106,51 +103,9 @@ contract DappDevMint
    */
    function removeDelegate(address oldDelegate) public {
       require(isOwner(msg.sender));
-      require(isDelegate(oldDelegate));
-      removeByValue(oldDelegate);
+      canMint[oldDelegate] = false;
+      emit AccessControl(oldDelegate, false);
   }
 
 
-    // helper functions for working with delegate array
-    /**
-     * @dev Function Find delegate by address
-     * @param delegate Address to find
-     */
-    function find(address delegate) public constant returns(uint) {
-        uint i = 0;
-        while (delegates[i] != delegate && i < delegates.length) {
-            i++;
-        }
-        return i; //@todo test and handle when address is NOT in array
-    }
-
-    /**
-     * @dev Function return address based on index
-     * @param index Index of delegate addresses to find
-     * @return address The address of delegate at index
-     */
-    function findByIndex(uint index) private constant returns(address) {
-        return delegates[index];
-    }
-
-    /**
-     * @dev Function remove address delegate array by address
-     * @param delegate Delegate address
-     */
-    function removeByValue(address delegate) private {
-        uint i = find(delegate);
-        removeByIndex(i);
-    }
-
-    /**
-     * @dev Function remove from array by index removeByValue
-     * @param i Index to removeByIndex
-     */
-    function removeByIndex(uint i) private {
-        while (i<delegates.length-1) {
-            delegates[i] = delegates[i+1];
-            i++;
-        }
-        delegates.length--;
-    }
 }
